0c08d8e58de7bacc8e36d7acf592e69dfe4eedfd
hyazhar <uddinmdazhar21@gmail.com>
2025-11-13 08:25:41 +0530

    Initial Commit

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 let hamburger = document.querySelector('.hamburger');
 let menus = document.querySelector('.menus');
 let extra_icons = document.querySelector('.extra-icon');
 hamburger.addEventListener('click', function() {
     menus.style.display = menus.style.display === 'block' ? 'none' : 'block';
     extra_icons.style.display = extra_icons.style.display === 'flex' ? 'none' : 'flex';
-=======
-let hamburger = document.querySelector('.hamburger');
-let menus = document.querySelector('.menus');
-let extra_icons = document.querySelector('.extra-icon');
-hamburger.addEventListener('click', function() {
-    menus.style.display = menus.style.display === 'block' ? 'none' : 'block';
-    extra_icons.style.display = extra_icons.style.display === 'flex' ? 'none' : 'flex';
->>>>>>> 23fae619d66a8a2b2e5471e386cf27ba9708c730
 });
